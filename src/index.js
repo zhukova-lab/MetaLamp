fa31247aa4730d9cad8508fa1f61aba9a740c13b
Zhukova Nina <avokadotata@gmail.com>
2021-10-15 18:31:09 +0300

    create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@fortawesome/fontawesome-free/js/all.js'
+import './styles/styles.less'
+import './styles/background.less'
+import './styles/fonts.css'
+import './images/background-main.jpg'
