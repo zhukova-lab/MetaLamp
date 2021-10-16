e94b35979ecfac220ec31f228ef1394991373945
Zhukova Nina <avokadotata@gmail.com>
2021-10-16 12:28:35 +0300

    create main for page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '@fortawesome/fontawesome-free/js/all.js'
 import './styles/styles.less'
-import './styles/background.less'
+import './styles/background-image.less'
 import './styles/fonts.css'
 import './images/background-main.jpg'
